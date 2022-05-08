278c3b59cf1aa210dd9782281a50dd0afc914961
igorpapr <igorpapr@gmail.com>
2022-05-08 23:04:15 +0300

    added backend

---
+++ backend/src/main/java/com/paprotskyi/fitnesslife/FitnessLifeApplication.java
@@ -0,0 +1,13 @@
+package com.paprotskyi.fitnesslife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessLifeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/paprotskyi/fitnesslife/FitnessLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paprotskyi.fitnesslife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
